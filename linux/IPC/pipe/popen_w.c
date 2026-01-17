9152c21a1bfd1607f64ab90d7b56751aaade2f96
subbench <dar06@qq.com>
2026-01-17 21:23:45 +0800

    多进程

--- linux/IPC/pipe/popen_w.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-
-    FILE *fp = popen("./scanf","w");
-    fwrite("5 6 ",1,3,fp);
-    pclose(fp);
-
-}
--- linux/IPC/pipe/scanf.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    int lhs, rhs;
-    scanf("%d%d", &rhs, &lhs);
-    printf("lhs + rhs = %d\n", lhs + rhs);
-}
